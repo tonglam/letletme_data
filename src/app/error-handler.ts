c619005ee00101ce677c25bd1d31340373155b31
tonglam <bluedragon00000@gmail.com>
2025-01-02 00:26:42 +0800

    Refactor error handling middleware; improve parameter naming for clarity. This commit updates the error handling middleware in the application by changing the parameter name from 'req' to '_' to indicate that the request object is not used. This small change enhances code readability and maintains consistency in the error handling logic.

--- src/app/error-handler.ts
+++ src/app/error-handler.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import { logger } from '../configs/app/app.config';
 
 export const setupErrorHandler = (app: express.Application): void => {
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
     logger.error({ err }, 'Internal Server Error');
     res.status(500).json({ error: 'Internal Server Error', details: err.message });
     next();
